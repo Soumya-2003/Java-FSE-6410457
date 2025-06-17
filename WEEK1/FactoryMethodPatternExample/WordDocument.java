d6947d8c1597fead39de078f38d5fc893ec4b5dd
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2025-06-17 20:02:02 +0530

    Add WordDocument class extending Document with openDoc and saveDoc methods

---
+++ WEEK1/FactoryMethodPatternExample/WordDocument.java
@@ -0,0 +1,11 @@
+
+public class WordDocument extends Document {
+
+    public void openDoc() {
+        System.out.println("\nOpening Word Document");
+    }
+
+    public void saveDoc() {
+        System.out.println("Saving Word Document");
+    }
+}
