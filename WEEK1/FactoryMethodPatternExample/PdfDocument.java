36773f869b5207d4c8244876738547ec01df0cdf
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2025-06-17 20:02:18 +0530

    Add PdfDocument class extending Document with openDoc and saveDoc methods

---
+++ WEEK1/FactoryMethodPatternExample/PdfDocument.java
@@ -0,0 +1,11 @@
+
+public class PdfDocument extends Document {
+
+    public void openDoc() {
+        System.out.println("\nOpening PDF Document");
+    }
+
+    public void saveDoc() {
+        System.out.println("Saving PDF Document");
+    }
+}
