a96827eb9207e9b1c878de6b5cbe366f0dbd2c86
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2025-06-17 20:02:29 +0530

    Add ExcelDocument class extending Document with openDoc and saveDoc methods

---
+++ WEEK1/FactoryMethodPatternExample/ExcelDocument.java
@@ -0,0 +1,10 @@
+public class ExcelDocument extends Document {
+
+    public void openDoc() {
+        System.out.println("\nOpening Excel Document");
+    }
+
+    public void saveDoc() {
+        System.out.println("Saving Excel Document");
+    }
+}
