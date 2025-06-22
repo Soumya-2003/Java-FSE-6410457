9981fcdacd34e5e389f9be232be22a098e48e30e
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2025-06-22 16:15:04 +0530

    Add App class with greet and add methods

---
+++ WEEK2/JUnit_Basic_Testing/setting_up_junit/src/main/java/com/lcwd/test/App.java
@@ -0,0 +1,12 @@
+package com.lcwd.test;
+
+public class App 
+{
+    public String greet(String name){
+        return "Hello " + name + "!";
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+}
