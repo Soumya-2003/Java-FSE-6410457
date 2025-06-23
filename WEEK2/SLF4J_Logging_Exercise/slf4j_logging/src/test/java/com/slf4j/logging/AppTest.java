ed88d4b7d9f7eec360dfdd2f6599502f4ba0d030
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2025-06-23 16:13:02 +0530

    Add unit test class AppTest for basic functionality verification

---
+++ WEEK2/SLF4J_Logging_Exercise/slf4j_logging/src/test/java/com/slf4j/logging/AppTest.java
@@ -0,0 +1,20 @@
+package com.slf4j.logging;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    private void assertTrue(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ WEEK2/SLF4J_Logging_Exercise/slf4j_logging/src/test/java/com/slf4j/logging/Test.java
@@ -0,0 +1,5 @@
+package com.slf4j.logging;
+
+public @interface Test {
+
+}
